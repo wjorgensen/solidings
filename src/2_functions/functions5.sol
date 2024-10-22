28e39d9b71fefb7926bd2655ab5a0ecb085643b4
Wezabis <wjorgensen29@gmail.com>
2024-10-22 14:38:09 -0400

    Started visibility

--- src/2_functions/functions5.sol
+++ src/2_functions/functions5.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {console2} from "forge-std/console2.sol";
-
 contract Functions5 {
-    function addOne(uint256 x) external pure returns(uint256) {
+    function addOne(uint256 x) public pure returns(uint256) {
         return x+1;
     }
 
